c3ee7ca845c82d82b95816f5dc53dafee37a5a22
Lamin Kinteh <kintehmuham92@gmail.com>
2026-07-14 09:59:31 -0400

    add another home tab

--- script.js
+++ script.js
@@ -185,3 +185,41 @@ themeToggle.innerHTML="🌙";
 }
 
 }
+
+//============================
+// ANIMATED COUNTERS
+//============================
+
+const counters = document.querySelectorAll(".counter");
+
+const speed = 80;
+
+counters.forEach(counter=>{
+
+const updateCount=()=>{
+
+const target=+counter.getAttribute("data-target");
+
+const count=+counter.innerText;
+
+const increment=Math.ceil(target/speed);
+
+if(count<target){
+
+counter.innerText=count+increment;
+
+setTimeout(updateCount,30);
+
+}
+
+else{
+
+counter.innerText=target;
+
+}
+
+}
+
+updateCount();
+
+});
